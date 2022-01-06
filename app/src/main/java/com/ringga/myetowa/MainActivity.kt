6e67134a475985e27585e36dbf4ee8782ffacf10
itdept2 <ringgadev@gmail.com>
2022-01-06 11:22:44 +0700

    update

--- app/src/main/java/com/ringga/myetowa/MainActivity.kt
+++ app/src/main/java/com/ringga/myetowa/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.ringga.myetowa
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.os.Handler
 import com.ringga.myetowa.databinding.ActivityMainBinding
 import com.ringga.myetowa.ui.auth.AuthActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
+    @SuppressLint("SimpleDateFormat", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,11 +24,19 @@ class MainActivity : AppCompatActivity() {
             openURL.data = Uri.parse("https://github.com/ringga-dev")
             startActivity(openURL)
         }
+        val sdf = SimpleDateFormat("dd-M-yyyy")
+        val currentDate = sdf.format(Date())
 
+        binding.tvDate.text = "Made with ❤️ from Indonesia \n $currentDate"
 
-        Snackbar.make(binding.root, "We changes app background color.", Snackbar.LENGTH_LONG).setAction("Undo") {
-
+        Handler().postDelayed({
             startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+        }, 4500)
+
+//        Snackbar.make(binding.root, "We changes app background color.", Snackbar.LENGTH_LONG).setAction("Undo") {
+//
+//            startActivity(Intent(this, AuthActivity::class.java))
 //            val snackbar = Snackbar.make(
 //                binding.root,
 //                "This is a styled snack bar.",
@@ -45,6 +57,6 @@ class MainActivity : AppCompatActivity() {
 //            }
 //
 //            snackbar.show()
-        }.show()
+//        }.show()
     }
 }
--- app/src/main/java/com/ringga/myetowa/ui/menu_home/product/fm/OrderFragment.kt
+++ app/src/main/java/com/ringga/myetowa/ui/menu_home/product/fm/OrderFragment.kt
@@ -1,10 +1,17 @@
 package com.ringga.myetowa.ui.menu_home.product.fm
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import com.ringga.myetowa.R
 import com.ringga.myetowa.databinding.FragmentOrderBinding
 
 
@@ -29,6 +36,43 @@ class OrderFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-
+        binding.openNewOrder.setOnClickListener {
+            showCustomAlert()
         }
+
+    }
+
+    private fun showCustomAlert() {
+
+        val inflate = layoutInflater
+        val infla_view = inflate.inflate(R.layout.alert_order, null)
+        val jadwal_visitor = infla_view.findViewById<CardView>(R.id.btn_jadwal_visitor)
+        val daftar_visitor = infla_view.findViewById<CardView>(R.id.btn_daftar_visitor)
+        val visitor_now = infla_view.findViewById<CardView>(R.id.btn_visitor_now)
+        val cencel = infla_view.findViewById<ImageView>(R.id.btn_cencel)
+        jadwal_visitor.setOnClickListener {
+//            startActivity(Intent(this, DaftarVisitorPlanActivity::class.java))
+        }
+
+        daftar_visitor.setOnClickListener {
+//            startActivity(Intent(this, DaftarVisitorActivity::class.java))
+        }
+
+        visitor_now.setOnClickListener {
+//            startActivity(Intent(this, DaftarVisitorBerjalanActivity::class.java))
+        }
+
+        val alertDialog = AlertDialog.Builder(requireContext())
+        alertDialog.setView(infla_view)
+        alertDialog.setCancelable(false)
+
+
+        val dialog = alertDialog.create()
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        cencel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+
+    }
 }
